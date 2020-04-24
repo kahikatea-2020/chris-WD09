7fb37c3947e8dfb9c9df2cf82189f237c3cd2a2b
albasychris <albasychris@gmail.com>
2020-04-24 20:34:08 +1200

    added my name from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,26 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+const contentful = require("contentful");
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "dg6tezt9ehsc",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "jzYHeJUGSrUSrMo-x4x5gnDFF7Mv1XjJdgL8R80pHNU"
+});
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+let name = null
+client
+  .getEntry("1EciK2tdf6WeBfFbjwkDtY")
+  .then(entry => {
+    console.log(entry.fields.names)
+    name = entry.fields.names
+  })
+  .catch(err => console.log(err));
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>{name}</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
